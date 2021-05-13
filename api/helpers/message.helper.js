71dbc1b429410cf50325000c418a0dab8a45704a
Pablo Andres Galvis Florez <pgalvis@makingsense.com>
2021-05-13 15:36:04 -0500

    chore: add unit test  y upgrade coverage

--- api/helpers/message.helper.js
+++ api/helpers/message.helper.js
@@ -1,11 +1,18 @@
-function handleErrorResponse(controller, method, response, result) {
-  response.status(409).send({
+function buildErrorResponse(nameController, nameMethod, result) {
+  const jsonResultError = {
     code: 409,
     message: result.error,
-    description: `Can't complete the request in ${controller}:${method}`,
-  });
+    description: `Can't complete the request in ${nameController}:${nameMethod}`,
+  };
+  return jsonResultError;
+}
+
+function handleErrorResponse(nameController, nameMethod, response, result) {
+  const jsonResultFailed = buildErrorResponse(nameController, nameMethod, result);
+  response.status(409).send(jsonResultFailed);
 }
 
 module.exports = {
+  buildErrorResponse, // Only test
   handleErrorResponse,
 };
